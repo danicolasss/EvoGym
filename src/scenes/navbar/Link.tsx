9d8d75dfb290b58c2349e5069b11a27d62bd98a1
dannynicolas.grangeo <danny-nicolas.grangeot@hotmail.com>
2023-02-11 11:04:21 +0100

    inti

---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,29 @@
+import { ValueTarget } from 'framer-motion';
+import React from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+type Props = {
+    page : string;
+    selectedPage: string;
+    setSelectedPage : (Value : string)=> void;
+}   
+
+const Link = ({
+    page,
+    selectedPage,
+    setSelectedPage,
+}: Props) => {
+    const lowerCasePage = page.toLowerCase().replace(/ /g, "");
+  return (
+    <AnchorLink 
+        href={`#${lowerCasePage}`}
+        className={`${selectedPage === lowerCasePage ? "text-primary-500": ""} transition duration-500 hover:text-primary-300`}
+        onClick={()=>setSelectedPage(lowerCasePage)} 
+    
+    >
+        {page}
+    </AnchorLink>
+  )
+}
+
+export default Link
